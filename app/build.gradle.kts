0bc1a8705af57aeac8b5861775b5ae8e27137fc6
Sam <sam.finchett@pm.me>
2026-09-22 21:08:04 +0900

    Android 0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "io.github.immersionplayer"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
         ndk {
             abiFilters += "arm64-v8a"
         }
